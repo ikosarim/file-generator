4bb792bbdeb7df04633ddcd482845561e8a91e10
Rocky-po <igor.kosarim@yandex.ru>
2017-05-15 17:25:12 +0300

    add method for sms string

--- src/main/java/ru/generate/sorm1/SmsSorm1.java
+++ src/main/java/ru/generate/sorm1/SmsSorm1.java
@@ -1,5 +1,7 @@
 package ru.generate.sorm1;
 
+import org.junit.Test;
+
 import java.lang.reflect.Field;
 
 /**
@@ -32,78 +34,32 @@ public class SmsSorm1 extends AllCommunications {
     //    22SSP
     public String smsMessage;
 
+    @Test
     public String printString() throws IllegalAccessException {
         SmsSorm1 sms = new SmsSorm1();
         Field[] fields = sms.getClass().getDeclaredFields();
         Field[] superFields = sms.getClass().getSuperclass().getDeclaredFields();
         StringBuilder textBuilder = new StringBuilder();
-        for (int j = 0; j <= 7; ++j) {
-            if (j < 4 || j == 5 || (j > 7 && j < 10) || (j > 14 && j < 19)) {
-                int count = 0;
-                for (Field superField : superFields) {
-                    ++count;
-                    if (count > 5) {
-                        superField.setAccessible(true);
-                        textBuilder.append(superField.get(sms)).append(";");
-                    } else if (count > 10) {
-                        break;
-                    }
-                }
-            } else if (j == 4) {
-                int count = 0;
-                for (Field field : fields) {
-                    ++count;
-                    if (count > 0) {
-                        field.setAccessible(true);
-                        textBuilder.append(field.get(sms)).append(";");
-                    } else if (count > 1) {
-                        break;
-                    }
-                }
-            } else if (j == 6 || j == 7) {
-                int count = 0;
-                for (Field field : fields) {
-                    ++count;
-                    if (count > 1) {
-                        field.setAccessible(true);
-                        textBuilder.append(field.get(sms)).append(";");
-                    } else if (count > 3) {
-                        break;
-                    }
-                }
-            } else if (j >= 10 && j <= 14) {
-                int count = 0;
-                for (Field field : fields) {
-                    ++count;
-                    if (count > 3) {
-                        field.setAccessible(true);
-                        textBuilder.append(field.get(sms)).append(";");
-                    } else if (count > 8) {
-                        break;
-                    }
-                }
-            } else if (j >= 19 && j <= 22) {
-                int count = 0;
-                for (Field field : fields) {
-                    ++count;
-                    if (count > 8) {
-                        field.setAccessible(true);
-                        textBuilder.append(field.get(sms)).append(";");
-                    }
-                }
-            }
-        }
+        addStringFragment(textBuilder, 0, 3, superFields, sms);
+        addStringFragment(textBuilder, 0, 1, fields, sms);
+        addStringFragment(textBuilder, 3, 4, superFields, sms);
+        addStringFragment(textBuilder, 1, 3, fields, sms);
+        addStringFragment(textBuilder, 4, 6, superFields, sms);
+        addStringFragment(textBuilder, 3, 8, fields, sms);
+        addStringFragment(textBuilder, 6, 100, superFields, sms);
+        addStringFragment(textBuilder, 8, 100, fields, sms);
+        System.out.println(textBuilder.toString());
         return textBuilder.toString();
     }
 
     private StringBuilder addStringFragment(StringBuilder textBuilder, int startCount, int stopCount, Field[] fields, Object obj) throws IllegalAccessException {
         int count = 0;
-        for (Field field:fields) {
+        for (Field field : fields) {
             ++count;
-            if (count > startCount){
+            if (count > startCount) {
                 field.setAccessible(true);
                 textBuilder.append(field.get((SmsSorm1) obj)).append(";");
-            } else if (count > stopCount){
+            } else if (count > stopCount) {
                 break;
             }
         }

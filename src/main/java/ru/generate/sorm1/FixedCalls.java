4560853bb3585b293bacca01962508e12cd8e4b0
Rocky-po <igor.kosarim@yandex.ru>
2017-05-17 12:26:48 +0300

    methods for fix and mobile

--- src/main/java/ru/generate/sorm1/FixedCalls.java
+++ src/main/java/ru/generate/sorm1/FixedCalls.java
@@ -3,7 +3,7 @@ package ru.generate.sorm1;
 /**
  * Created by kosarim on 5/3/17.
  */
-public class FixedCalls extends AbstractCall {
+public class FixedCalls extends AbstractCall implements iCdr {
 
     //    11FSP
     public String pnASignCount = "11";
@@ -18,4 +18,20 @@ public class FixedCalls extends AbstractCall {
                 operationCode + serviceCode + serviceDescr + serviceAddCode + addCode;
     }
 
+
+    @Override
+    public String createCallMessage() {
+        StringBuilder fixCallMessage = new StringBuilder();
+        for (int i = 0; i < Integer.parseInt(this.code); i++) {
+            fixCallMessage.append(this.toString());
+            if (Integer.parseInt(this.code) == 66){
+                this.code = "70";
+            } else if (Integer.parseInt(this.code) == 70){
+                this.code = "67";
+            } else {
+                this.code = String.valueOf(Integer.parseInt(this.code) + 1);
+            }
+        }
+        return fixCallMessage.toString();
+    }
 }
